f92220636028070d0e3b6e05867248b16626502e
nicolasacevedopezo <nicolas.acevedo.pezo@gmail.com>
2026-03-16 19:43:37 -0300

    prueba

--- E1-M6 Ejercicio/app.js
+++ E1-M6 Ejercicio/app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
 
-    res.end('¡Hola Mundo!');
+    res.end('Hola Mundo');
 });
 
 const PORT = 3000;
